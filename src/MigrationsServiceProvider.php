310b0001fd13a78cf8d49f82b847fdf81fbd5ae4
Alex Gray <ajm_gray@yahoo.com>
2022-04-19 10:13:08 +0100

    sync-metadata-storage

--- src/MigrationsServiceProvider.php
+++ src/MigrationsServiceProvider.php
@@ -17,6 +17,7 @@ use LaravelDoctrine\Migrations\Console\ResetCommand;
 use LaravelDoctrine\Migrations\Console\RollbackCommand;
 use LaravelDoctrine\Migrations\Console\StatusCommand;
 use LaravelDoctrine\Migrations\Console\VersionCommand;
+use LaravelDoctrine\Migrations\Console\SyncMetadataStorageCommand;
 
 class MigrationsServiceProvider extends ServiceProvider
 {
@@ -56,7 +57,8 @@ class MigrationsServiceProvider extends ServiceProvider
             VersionCommand::class,
             RefreshCommand::class,
             RollbackCommand::class,
-            GenerateCommand::class
+            GenerateCommand::class,
+            SyncMetadataStorageCommand::class
         ]);
     }
 
